b144fff448f900d8fa9d44dfac8ad29799c29b69
Colin Cummings <colin_cummings@brown.edu>
2019-03-26 20:14:02 -0400

    add and configure gatsby-plugin-feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,6 +63,7 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-feed`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-typography`,
